29b1dd04304edec1e3fc7ecf9bb61e98d4eacb7f
Benedictify <benedictify@gmail.com>
2019-05-19 17:49:39 -0500

    add carousel methods

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -12,9 +12,10 @@ class Carousel {
 		this.rightBtn = this.carousElement.querySelector('.right-button');
 		this.imageArray = this.carousElement.querySelectorAll('img');
 		this.currentImg = this.imageArray[0]; // start with first one
-
+		this.currentIndex = 0; // always update currentIndex simultaneously with currentImg
+	}
 		/*
-		methods:
+		class methods:
 		- move left - subtract one from index, reassign currentImg to images[newIndex]
 		- move right - add one to index, reassign currentImg to images[newIndex]
 		
@@ -27,6 +28,37 @@ class Carousel {
 		- wrapRight - change currentImg from last index to first
 		*/
 
-		
+	setCurrentImgIndexTo(newIndex) {
+		// always update currentIndex simultaneously with currentImg
+		this.currentImg = this.imageArray[newIndex];
+		this.currentIndex = newIndex;
+	}
+	
+	isAtStart() {
+		return (this.currentIndex === 0);
+	}
+	
+	isAtEnd() {
+		return (this.currentIndex === this.imageArray.length - 1);
+	}
+	
+	moveLeft(){
+		if (this.isAtStart() === false) {
+			// if not at start, move left 1
+			setCurrentImgIndexTo(this.currentIndex - 1)
+		} else if (this.isAtStart() === true) {
+			// if at start, move to end
+			setCurrentImgIndexTo(this.imageArray.length - 1)
+		}
+	}
+
+	moveRight(){
+		if (this.isAtEnd() === false) {
+			// if not at end, move right 1
+			setCurrentImgIndexTo(this.currentIndex + 1)
+		} else if (this.isAtEnd() === true) {
+			// if at end, move to start
+			setCurrentImgIndexTo(0)
+		}
 	}
 }
